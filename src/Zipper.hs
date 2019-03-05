0c3fb97d620939647972924efae32a632c3be2b5
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-03-05 23:30:42 +0100

    Fix Zipper

--- src/Zipper.hs
+++ src/Zipper.hs
@@ -1,34 +1,36 @@
-module Zipper(Zipper, fromList, toList, zipLeft, zipRight, getOffset, setOffset, current) where
+module Zipper(Zipper, fromList, left, right , zipLeft, zipRight, getOffset, setOffset, current) where
 
 data Zipper a = Zipper [a] [a] deriving Show
 
 fromList :: [a] -> Zipper a
-fromList = flip Zipper []
+fromList r = Zipper [] r
 
-toList :: Zipper a -> [a]
-toList (Zipper l r) = l ++ (reverse r)
+left :: Zipper a -> [a]
+left (Zipper l _) = l
 
-zipLeft :: Zipper a -> Zipper a
-zipLeft (Zipper l r) = Zipper (head r : l) (tail r)
+right :: Zipper a -> [a]
+right (Zipper _ r) = reverse r
 
 zipRight :: Zipper a -> Zipper a
-zipRight (Zipper l r) = Zipper (tail l) (head l : r)
+zipRight (Zipper l r) = Zipper (head r : l) (tail r)
+
+zipLeft :: Zipper a -> Zipper a
+zipLeft (Zipper l r) = Zipper (tail l) (head l : r)
 
 current :: Zipper a -> a
-current (Zipper l _) = head l
+current (Zipper _ r) = head r
 
 getOffset :: Zipper a -> Int
-getOffset (Zipper _ r) = length r
+getOffset (Zipper l _) = length l
 
-setOffset :: Zipper a -> Int -> Zipper a
-setOffset z n
-    | d < 0     = goRight z (abs d)
-    | otherwise = goLeft z d
+setOffset :: Int -> Zipper a -> Zipper a
+setOffset n z
+    | d < 0     = goLeft  (abs d) z
+    | otherwise = goRight d       z
   where
     d = n - getOffset z
 
-    goRight z 0 = z
-    goRight z n = goRight (zipRight z) (n - 1)
-
-    goLeft z 0 = z
-    goLeft z n = goLeft (zipLeft z) (n - 1)
+    goLeft 0 z  = z
+    goLeft n z  = goLeft (n - 1) (zipLeft z)
+    goRight 0 z = z
+    goRight n z = goRight (n - 1) (zipRight z)
